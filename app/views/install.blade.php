798c86690659ffd7ce69541cc3f801e4ee09ec7a
Alexander Skjolden <alex@plastboks.net>
2014-02-18 08:32:33 +0100

    added surname and givenname to install template
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- app/views/install.blade.php
+++ app/views/install.blade.php
@@ -14,6 +14,14 @@ Install
         <p>{{ Form::label('email', 'Email') }}</p>
         {{ $errors->first('email', '<p class="error">:message</p>') }}
         <p>{{ Form::text('email', $email) }}</p>
+        <!-- body field -->
+        <p>{{ Form::label('givenname', 'Givenname') }}</p>
+        {{ $errors->first('givenname', '<p class="error">:message</p>') }}
+        <p>{{ Form::text('givenname', $givenname) }}</p>
+        <!-- body field -->
+        <p>{{ Form::label('surname', 'Surname') }}</p>
+        {{ $errors->first('surname', '<p class="error">:message</p>') }}
+        <p>{{ Form::text('surname', $surname) }}</p>
         <!-- password field -->
         <p>{{ Form::label('password', 'Password') }}</p>
         {{ $errors->first('password', '<p class="error">:message</p>') }}
